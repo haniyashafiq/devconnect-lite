f97abf2a75f0608e4e36115e456be6179d9eb547
haniyashafiq <haniyashafiq2005@gmail.com>
2025-09-29 05:09:16 -0700

    projects export

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,4 +9,3 @@ router.post("/login", login);
 
 export default router
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6InVzZXIiLCJpYXQiOjE3NTkxNDM2NDEsImV4cCI6MTc1OTIzMDA0MX0.LWEnuZiL1xa1lw2br8qj827NaPX_4TIFswlgZ99bmG4
